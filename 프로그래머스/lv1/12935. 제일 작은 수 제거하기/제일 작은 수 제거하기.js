d8a7b54c6d9f035a29531aea0e7afff821cafe92
Jzzyy <121776954+Jzzyy@users.noreply.github.com>
2023-04-10 10:36:14 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
@@ -0,0 +1,10 @@
+function solution(arr) {
+   if (arr.length <= 1) {
+    return [-1];
+  } else {
+    const min = Math.min(...arr);
+    const minIndex = arr.indexOf(min);
+    arr.splice(minIndex, 1);
+    return arr;
+  }
+}
