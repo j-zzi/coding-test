941f61bd937d8682a5689eff9a648f355f3932b9
Jzzyy <121776954+Jzzyy@users.noreply.github.com>
2023-05-09 14:42:22 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.js
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.js
@@ -1,13 +1,10 @@
 function solution(food) {
   let result = [];
-  let second = [];
   food.forEach((element, index) => {
     if (index !== 0) {
-      result.push(String(index).repeat(parseInt(element / 2)));
+      result += String(index).repeat(parseInt(element / 2));
     }
   });
-  second.push(...result);
-  second.push(0);
-  second.push(...result.reverse());
-  return second.join("");
+  return result + 0 + [...result].reverse().join("");
+
 }
