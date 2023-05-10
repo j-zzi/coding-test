cb52a9137b73d64cc7f5305ffaf3a73da1721d7d
Jzzyy <121776954+Jzzyy@users.noreply.github.com>
2023-05-10 14:15:15 +0900

    [level 1] Title: 2016년, Time: 0.15 ms, Memory: 33.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12901. 2016년/2016년.js
@@ -0,0 +1,7 @@
+function solution(a, b) {
+  const days = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+  const date = new Date(`2016-${a}-${b}`);
+  const dayOfWeek = date.getDay();
+
+  return days[dayOfWeek];
+}
