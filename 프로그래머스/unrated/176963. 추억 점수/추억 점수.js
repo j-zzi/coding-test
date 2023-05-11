6f2dce5b20b7fcecc92bf989ecd7accce51d9485
Jzzyy <121776954+Jzzyy@users.noreply.github.com>
2023-05-11 10:55:39 +0900

    [unrated] Title: 추억 점수, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/176963. 추억 점수/추억 점수.js
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.js
@@ -1,16 +1,9 @@
 function solution(name, yearning, photo) {
-  let match = new Map();
-  let sum = 0;
-  let result = [];
+  let year = new Map();
   for (i = 0; i < name.length; i++) {
-    match.set(name[i], yearning[i]);
+    year.set(name[i], yearning[i]);
   }
-  for (i = 0; i < photo.length; i++) {
-    for (j = 0; j < photo[i].length; j++) {
-      if (match.get(photo[i][j])) sum += match.get(photo[i][j]);
-    }
-    result.push(sum);
-    sum = 0;
-  }
-  return result;
+  return photo.map((names) =>
+    names.reduce((result, n) => result + (year.get(n) || 0), 0)
+  );
 }
