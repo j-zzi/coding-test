709dcfef27446a9ac68188e0d4f1f217712e19cf
jzzy <121776954+LeeeeeeJH@users.noreply.github.com>
2023-05-18 12:25:23 +0900

    [unrated] Title: 과일 장수, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/135808. 과일 장수/과일 장수.js
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.js
@@ -1,12 +1,8 @@
 function solution(k, m, score) {
-  let arr = [];
   let result = 0;
-  score.sort((a, b) => b - a).unshift(0);
-  for (i = 1; i < score.length - m + 1; i += m) {
-    arr.push(score.slice(i, i + m));
-  }
-  for (i = 0; i < arr.length; i++) {
-    result += Math.min(...arr[i]) * m;
+  score.sort((a, b) => b - a);
+  for (i = 0; i <= score.length - m; i += m) {
+    result += score[i + m - 1] * m;
   }
   return result;
 }
