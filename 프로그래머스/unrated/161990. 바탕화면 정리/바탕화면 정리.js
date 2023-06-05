50992bebd6f154ba2569624a5843885406686234
jzzy <121776954+LeeeeeeJH@users.noreply.github.com>
2023-06-05 15:01:23 +0900

    [unrated] Title: 바탕화면 정리, Time: 1.02 ms, Memory: 34 MB -BaekjoonHub

--- 프로그래머스/unrated/161990. 바탕화면 정리/바탕화면 정리.js
+++ 프로그래머스/unrated/161990. 바탕화면 정리/바탕화면 정리.js
@@ -1,30 +1,4 @@
 function solution(wallpaper) {
-  const filesPosition = {
-    row: [],
-    col: [],
-  };
-  const result = new Array();
-
-  const insertFilesPosition = (wallpaper) => {
-    for (const [rowIndex, row] of wallpaper.entries()) {
-      for (const [colIndex, col] of [...row].entries()) {
-        if (col === "#") {
-          filesPosition["row"].push(rowIndex);
-          filesPosition["col"].push(colIndex);
-        }
-      }
-    }
-  };
-
-  const insertOuterPosition = (filesPosition) => {
-    result[0] = Math.min(...filesPosition["row"]);
-    result[1] = Math.min(...filesPosition["col"]);
-    result[2] = Math.max(...filesPosition["row"])+1;
-    result[3] = Math.max(...filesPosition["col"])+1;
-  };
-
-  insertFilesPosition(wallpaper);
-  insertOuterPosition(filesPosition);
-
-  return result;
+    var answer = [];
+    return answer;
 }
