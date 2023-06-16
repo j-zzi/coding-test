290bd3b7ded7ece639130cdcfb25ad42abf0f06e
jzzy <121776954+LeeeeeeJH@users.noreply.github.com>
2023-06-16 12:20:59 +0900

    [level 2] Title: 피보나치 수, Time: 40.04 ms, Memory: 41 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.js
@@ -0,0 +1,8 @@
+function solution(n) {
+  let sequence = [0, 1];
+
+  for (let i = 0; sequence.length <= n; i++) {
+    sequence.push(sequence.slice(-2).reduce((acc, cur) => acc + cur, 0)%1234567);
+  }
+  return sequence[n] % 1234567;
+}
