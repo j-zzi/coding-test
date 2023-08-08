ff98daa8e009cccce1993031408e6e3d9ace79f1
jzzy <121776954+j-zzi@users.noreply.github.com>
2023-08-08 11:09:04 +0900

    [level 2] Title: 기능개발, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.js
@@ -0,0 +1,25 @@
+function solution(progresses, speeds) {
+  const answer = [];
+  const developAccesses = progresses.map((progress, idx) =>
+    Math.ceil((100 - progress) / speeds[idx])
+  );
+
+  let developedDay = developAccesses[0];
+  let developedDayCount = 0;
+
+  developAccesses.forEach((developAccess) => {
+    if (developedDay >= developAccess) {
+      developedDayCount++;
+    } else {
+      developedDay = developAccess;
+      answer.push(developedDayCount);
+      developedDayCount = 1;
+    }
+  });
+
+  if (developedDayCount) {
+    answer.push(developedDayCount);
+  }
+
+  return answer;
+}
