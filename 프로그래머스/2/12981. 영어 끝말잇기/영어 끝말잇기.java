29d36f87d0ee210f9e29a6f6c4741b00ae7cc87c
jzzy <121776954+j-zzi@users.noreply.github.com>
2024-10-25 14:03:22 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.16 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -3,21 +3,21 @@ import java.util.*;
 class Solution {
     public int[] solution(int n, String[] words) {
         int[] answer = {0,0};
-        Stack<String> usedWords = new Stack<>();
-        usedWords.push(words[0]);
+        HashSet<String> usedWords = new HashSet<>();
+        char lastWord = words[0].charAt(words[0].length() - 1);
+        usedWords.add(words[0]);
 
         for (int i = 1; i < words.length; i++) {
-            char lastWord = usedWords.peek().charAt(words[i-1].length() - 1);
             char firstWord = words[i].charAt(0);
+            usedWords.add(words[i]);
 
-            if (lastWord != firstWord || usedWords.contains(words[i])) {
+            if (usedWords.size() != i + 1 || lastWord != firstWord) {
                 answer[0] = i % n + 1;
                 answer[1] = i / n + 1;
 
                 return answer;
             }
-
-            usedWords.push(words[i]);
+            lastWord = words[i].charAt(words[i].length() - 1);
         }
 
         return answer;
