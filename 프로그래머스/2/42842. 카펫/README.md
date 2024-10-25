8e9b96816245bce404523427e39b6d37f00e8ba4
jzzy <121776954+j-zzi@users.noreply.github.com>
2024-10-25 13:28:46 +0900

    [level 2] Title: 카펫, Time: 0.20 ms, Memory: 85.4 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/README.md
+++ 프로그래머스/2/42842. 카펫/README.md
@@ -1,10 +1,10 @@
 # [level 2] 카펫 - 42842 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42842?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42842) 
 
 ### 성능 요약
 
-메모리: 73.3 MB, 시간: 0.03 ms
+메모리: 85.4 MB, 시간: 0.20 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 26일 16:10:56
+2024년 10월 25일 13:28:41
 
 ### 문제 설명
 
--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,23 +1,25 @@
-class Solution {
-    public int[] solution(int brown, int yellow) {
+class Solution
+{
+    public int[] solution(int brown,int yellow)
+    {
         int[] answer = new int[2];
         int sum = brown + yellow;
-        
-        for (int i = 3; i < sum; i++) {
-            int j = sum / i;
-            
-            if (sum % i == 0 && j >= 3) {
-                int col = Math.max(i, j);
-                int row = Math.min(i, j);
+
+        for (int col = 3; col < sum; col++) {
+            int row = sum / col;
+
+            if (sum % row == 0 && col >= row) {
                 int center = (col - 2) * (row - 2);
-                
+
                 if (center == yellow) {
                     answer[0] = col;
                     answer[1] = row;
+
                     return answer;
                 }
             }
         }
+
         return answer;
     }
 }
