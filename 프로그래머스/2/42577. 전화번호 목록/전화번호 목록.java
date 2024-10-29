0558616eab4346b50eb60dc2d742a4021ec947a9
jzzy <121776954+j-zzi@users.noreply.github.com>
2024-10-29 20:12:20 +0900

    [level 2] Title: 전화번호 목록, Time: 276.06 ms, Memory: 221 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public boolean solution(String[] phone_book) {
+        boolean answer = true;
+        HashMap<String, Integer> map = new HashMap<>();
+
+        for (String phone : phone_book) {
+            map.put(phone, 0);
+        }
+
+        for (int i = 0; i < phone_book.length; i++) {
+            for (int j = 0; j < phone_book[i].length(); j++) {
+                if (map.containsKey(phone_book[i].substring(0, j))) {
+                    return false;
+                }
+            }
+        }
+
+        return answer;
+    }
+}
