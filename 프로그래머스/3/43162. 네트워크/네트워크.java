f6103cc3ba52de0b13bc5a379998815c93c131a4
jzzy <121776954+j-zzi@users.noreply.github.com>
2024-10-31 12:57:38 +0900

    [level 3] Title: 네트워크, Time: 0.51 ms, Memory: 74 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int solution(int n, int[][] computers) {
+        int answer = 0;
+        boolean[] visited = new boolean[n];
+
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                answer++;
+                dfs(i, visited, computers);
+            }
+        }
+
+        return answer;
+    }
+
+    public void dfs(int node, boolean[] visited, int[][] computers) {
+        visited[node] = true;
+
+        for (int i = 0; i < computers[node].length; i++) {
+            if (i != node && !visited[i] && computers[node][i] == 1) {
+                dfs(i, visited, computers);
+            }
+        }
+    }
+}
