ef47253ccd969da70486576b64c54726a0986dfb
jzzy <121776954+j-zzi@users.noreply.github.com>
2024-10-31 13:09:39 +0900

    [level 3] Title: 네트워크, Time: 0.48 ms, Memory: 80.6 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,3 +1,4 @@
+import java.util.*;
 class Solution {
     public int solution(int n, int[][] computers) {
         int answer = 0;
@@ -6,7 +7,8 @@ class Solution {
         for (int i = 0; i < n; i++) {
             if (!visited[i]) {
                 answer++;
-                dfs(i, visited, computers);
+//                dfs(i, visited, computers);
+                bfs(i, visited, computers);
             }
         }
 
@@ -19,6 +21,24 @@ class Solution {
         for (int i = 0; i < computers[node].length; i++) {
             if (i != node && !visited[i] && computers[node][i] == 1) {
                 dfs(i, visited, computers);
+            }
+        }
+    }
+
+    public void bfs(int node, boolean[] visited, int[][] computers) {
+        visited[node] = true;
+
+        Queue<Integer> queue = new LinkedList<>();
+
+        queue.offer(node);
+
+        while (!queue.isEmpty()) {
+            int currentNode = queue.poll();
+            for (int i = 0; i < visited.length; i++) {
+                if (i != currentNode && !visited[i] && computers[currentNode][i] == 1) {
+                    visited[i] = true;
+                    queue.offer(i);
+                }
             }
         }
     }
