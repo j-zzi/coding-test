654a8d0019451caf43e58b4c8e4b10df4efbb3fa
jzzy <121776954+j-zzi@users.noreply.github.com>
2024-11-01 17:18:25 +0900

    [level 3] Title: 단어 변환, Time: 0.61 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,49 +1,48 @@
 import java.util.*;
 
 class Solution {
-    static class Node {
+    class Node{
         String word;
-        int count;
+        int step;
 
-        public Node(String word, int count) {
+        public Node(String word, int step) {
             this.word = word;
-            this.count = count;
+            this.step = step;
         }
     }
+    public int answer = 0;
     public int solution(String begin, String target, String[] words) {
         boolean[] visited = new boolean[words.length];
-
         return bfs(begin, target, visited, words);
     }
 
     public int bfs(String begin, String target, boolean[] visited, String[] words) {
         Queue<Node> queue = new LinkedList<>();
-
         queue.offer(new Node(begin, 0));
 
         while (!queue.isEmpty()) {
             Node currentNode = queue.poll();
+            String currentWord = currentNode.word;
+            int currentStep = currentNode.step;
 
-            if (currentNode.word.equals(target)) return currentNode.count;
+            if (currentWord.equals(target) ) {
+                return currentStep;
+            }
 
-            for (int i = 0; i < visited.length; i++) {
-                if (!visited[i] && canChange(currentNode.word, words[i])) {
+            for (int i = 0; i < words.length; i++) {
+                if (canChange(currentWord, words[i]) && !visited[i]) {
                     visited[i] = true;
-                    queue.offer(new Node(words[i], currentNode.count + 1));
+                    queue.offer(new Node(words[i], currentStep + 1));
                 }
             }
         }
         return 0;
     }
 
-    private static boolean canChange(String word1, String word2) {
+    private boolean canChange(String word1, String word2) {
         int diffCount = 0;
-
         for (int i = 0; i < word1.length(); i++) {
-            if (word1.charAt(i) != word2.charAt(i)) {
-                diffCount++;
-                if (diffCount > 1) return false;
-            }
+            if (word1.charAt(i) != word2.charAt(i)) diffCount++;
         }
 
         return diffCount == 1;
