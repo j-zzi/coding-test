03417c58f355ce4ab04fb7c8063b455b3e05d019
jzzy <121776954+j-zzi@users.noreply.github.com>
2024-11-06 19:03:53 +0900

    [level 2] Title: 땅따먹기, Time: 25.90 ms, Memory: 86.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
@@ -0,0 +1,25 @@
+class Solution {
+    int solution(int[][] land) {
+        int answer = 0;
+
+        int[][] dp = new int[land.length + 1][4];
+
+        for (int i = 1; i <= land.length; i++) {
+            for (int j = 0; j < 4; j++) {
+                for (int k = 0; k < 4; k++) {
+                    if (j == k) {
+                        continue;
+                    }
+
+                    dp[i][j] = Math.max(dp[i][j], land[i - 1][j] + dp[i - 1][k]);
+
+                    answer = Math.max(dp[i][j], answer);
+                }
+            }
+        }
+
+
+
+        return answer;
+    }
+}
