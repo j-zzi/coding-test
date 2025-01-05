88a08b0455a21236f4a503bb2f9e549a9340d42e
jzzy <121776954+j-zzi@users.noreply.github.com>
2025-01-05 09:22:27 +0900

    [level 2] Title: 줄 서는 방법, Time: 0.05 ms, Memory: 53.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n, long k) {
+        List<Integer> list = new ArrayList<>();
+        int[] answer = new int[n];
+
+        long factorial = 1;
+
+        for (int i = 1; i <= n; i++) {
+            factorial *= i;
+            list.add(i);
+        }
+
+        k--;
+
+        int idx = 0;
+        while (idx < n) {
+            factorial /= n - idx;
+
+            answer[idx++] = list.remove((int) (k / factorial));
+
+            k %= factorial;
+        }
+
+        return answer;
+    }
+}
