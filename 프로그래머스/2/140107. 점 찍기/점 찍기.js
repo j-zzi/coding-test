72632a2b6da2daa8d2aeb2282a50e5ce491ec332
jzzy <121776954+j-zzi@users.noreply.github.com>
2025-02-07 14:31:21 +0900

    [level 2] Title: 점 찍기, Time: 28.03 ms, Memory: 37.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/140107. 점 찍기/점 찍기.js
@@ -0,0 +1,9 @@
+function solution(k, d) {
+  let answer = 0;
+  for (let i = 0; i <= d; i += k) {
+    const y = Math.sqrt(d ** 2 - i ** 2);
+    answer += Math.floor(y / k) + 1;
+  }
+
+  return answer;
+}
